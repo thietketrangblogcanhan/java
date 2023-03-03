422bef59ed1fc64f5e9b90c31d2bdba47aa53e24
QuanPC2k2 <anhquanizone2910@gmail.com>
2023-03-03 15:25:26 +0700

    upload

---
+++ Bai1/src/bai1/UCLN.java
@@ -0,0 +1,31 @@
+
+package bai1;
+
+import java.util.Scanner;
+
+public class UCLN {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Nhập số thứ nhất: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Nhập số thứ hai: ");
+        int num2 = input.nextInt();
+
+        int gcd = findGCD(num1, num2);
+
+        System.out.println("Ước chung lớn nhất của " + num1 + " và " + num2 + " là: " + gcd);
+    }
+
+    public static int findGCD(int num1, int num2) {
+        while (num1 != num2) {
+            if (num1 > num2) {
+                num1 = num1 - num2;
+            } else {
+                num2 = num2 - num1;
+            }
+        }
+        return num1;
+    }
+}
